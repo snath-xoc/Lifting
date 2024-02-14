b812c93fd2ae5794e9216f85c26404167f196598
snath-xoc <51656807+snath-xoc@users.noreply.github.com>
2024-02-14 19:24:12 +0100

    Update config.py

--- load_data/config.py
+++ load_data/config.py
@@ -4,10 +4,10 @@ def get_config():
     
     config = {
         'dir_meta_data': '',
-        'dir_cmip6_data': '/mnt/CMIP6_storage/cmip6-ng/',
-        'dir_output': '/mnt/PROVIDE/shruti/WBGT/tas/',
-        'dir_output_ssp585': '/home/shruti/WBGT/tas/ssp585/',
-        'dir_output_hurs': '/mnt/PROVIDE/shruti/WBGT/hurs/'
+        'dir_cmip6_data': 'UKESM1-0-LL/',
+        'dir_output': '/WBGT/tas/',
+        'dir_output_ssp585': '/WBGT/tas/ssp585/',
+        'dir_output_hurs': '/WBGT/hurs/'
     }
     
     return config
